b5e9ec91bff4c56e5f497e59cd304684a4814cf1
ArtiomPatov <patov.988@gmail.com>
2021-10-11 23:48:06 +0300

    added reworked version of task 3 homework 4 (with tests)

--- Hw4Task3/Hw4Task3/Hw4Task3.c
+++ Hw4Task3/Hw4Task3/Hw4Task3.c
@@ -6,10 +6,18 @@ typedef struct {
     char phone[30];
 } PhoneBookEntry;
 
-// function for reading data from file at the beginning of the program
-int readDataFromFile(PhoneBookEntry* notesArray, int currentIndex)
+// function for reading data from file at the beginning of the program; testOrNot = true => opens test.txt, else opens data.txt
+int readDataFromFile(PhoneBookEntry* notesArray, int currentIndex, bool testOrNot)
 {
-    FILE* notes = fopen("data.txt", "r");
+    FILE* notes;
+    if (testOrNot)
+    {
+        notes = fopen("test.txt", "r");
+    }
+    else
+    {
+        notes = fopen("data.txt", "r");
+    }
     if (notes == NULL)
     {
         printf("File not found");
@@ -41,18 +49,10 @@ int readDataFromFile(PhoneBookEntry* notesArray, int currentIndex)
 }
 
 // function for adding note (name and phone number) to an array with all notes
-void addNote(PhoneBookEntry* notesArray, int currentIndex)
+void addNote(PhoneBookEntry* notesArray, char* name, char* phone, int currentIndex)
 {
-    char* name[30];
-    printf("\nName: ");
-    fgets(name, 30, stdin);
     strcpy(notesArray[currentIndex].name, name);
-
-    char* phone[30];
-    printf("Phone number: ");
-    fgets(phone, 30, stdin);
     strcpy(notesArray[currentIndex].phone, phone);
-
     return;
 }
 
@@ -68,46 +68,61 @@ void printAllNotes(PhoneBookEntry* notesArray, int currentIndex)
 }
 
 // function for searching phone number using name
-void findPhoneNumberUsingName(PhoneBookEntry* notesArray, int currentIndex)
+int findPhoneNumberUsingName(PhoneBookEntry* notesArray, char* toSearchName, int currentIndex)
 {
-    char* toSearchName[30];
-    printf("\nName: ");
-    fgets(toSearchName, 30, stdin);
+    int indexWithWantedNote = -1;
     for (int index = 0; index < currentIndex; index++)
     {
         if (!strcmp(notesArray[index].name, toSearchName))
         {
-            printf("Phone number: %s", notesArray[index].phone);
-            return;
+            indexWithWantedNote = index;
+            return indexWithWantedNote;
         }
     }
-    printf("There is no person with this name\n");
-    return;
+    return indexWithWantedNote;
 }
 
 // function for searching name using phone number
-void findNameUsingPhoneNumber(PhoneBookEntry* notesArray, int currentIndex)
+int findNameUsingPhoneNumber(PhoneBookEntry* notesArray, char* toSearchPhoneNumber, int currentIndex)
 {
-    char* toSearchPhoneNumber[30];
-    printf("\nPhone number: ");
-    fgets(toSearchPhoneNumber, 30, stdin);
+    int indexWithWantedNote = -1 ;
     for (int index = 0; index < currentIndex; index++)
     {
         if (!strcmp(notesArray[index].phone, toSearchPhoneNumber))
         {
-            printf("Name: %s", notesArray[index].name);
-            return;
+            indexWithWantedNote = index;
+            return indexWithWantedNote;
         }
     }
-    printf("There is no person with this phone number\n");
-    return;
+    return indexWithWantedNote;
+}
+
+// assistant function for two searching functions; works this way: when variable nameOrPhone is true it prints name, when false -- phone
+void printNameOrPhoneNumber(PhoneBookEntry* notesArray, int indexWithWantedNote, bool nameOrPhone)
+{
+    if (nameOrPhone)
+    {
+        printf("Name: %s", notesArray[indexWithWantedNote].name);
+    }
+    else
+    {
+        printf("Phone number: %s", notesArray[indexWithWantedNote].phone);
+    }
 }
 
 // function for saving data to file
-void saveData(PhoneBookEntry* notesArray, int currentIndex)
+void saveData(PhoneBookEntry* notesArray, int currentIndex, bool testOrNot)
 {
-    FILE* notes= fopen("data.txt", "w");
-    if (notes == NULL)  
+    FILE* notes;
+    if (testOrNot)
+    {
+        notes = fopen("test.txt", "w");
+    }
+    else
+    {
+        notes = fopen("data.txt", "w");
+    }
+    if (notes == NULL)
     {
         printf("File not found");
         return;
@@ -120,39 +135,167 @@ void saveData(PhoneBookEntry* notesArray, int currentIndex)
     return;
 }
 
-void main()
+bool compareTwoArraysOfStruct(PhoneBookEntry* firstArray, PhoneBookEntry* secondArray, int currentIndex)
 {
+    for (int index = 0; index < currentIndex; index++)
+    {
+        if (strcmp(firstArray[index].name, secondArray[index].name) != 0)
+        {
+            return false;
+        }
+    }
+    for (int index = 0; index < currentIndex; index++)
+    {
+        if (strcmp(firstArray[index].phone, secondArray[index].phone) != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool testReadDataFromFile(PhoneBookEntry* testArray)
+{
+    readDataFromFile(testArray, 0, true);
+    char* arrayWithTestValues[4] = { "isaac newton\n", "051-6320-123\n", "rene descartes\n", "03240-1234-534\n" };
+    for (int index = 0; index < 3; index += 2)
+    {
+        if (strcmp(testArray[index/2].name, arrayWithTestValues[index]) != 0)
+        {
+            return false;
+        }
+    }
+    for (int index = 1; index < 4; index += 2)
+    {
+        if (strcmp(testArray[index/2].phone, arrayWithTestValues[index]) != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool testAddNote(PhoneBookEntry* testArray)
+{
+    const char* name = "Adam Smith\n";
+    const char* phone = "7(911)-123-45-67\n";
+    addNote(testArray, name, phone, 2);
+    return (!strcmp(testArray[2].name, "Adam Smith\n") && !strcmp(testArray[2].phone, "7(911)-123-45-67\n"));
+}
+
+bool testSearchFunctions(PhoneBookEntry* testArray)
+{
+    const char* name = "Adam Smith\n";
+    return (findPhoneNumberUsingName(testArray, "Adam Smith\n", 3) == 2 && findNameUsingPhoneNumber(testArray, "03240-1234-534\n", 3) == 1);
+}
+
+bool testSaveDataToFile(PhoneBookEntry* testArray)
+{
+    saveData(testArray, 3, true);
+    readDataFromFile(testArray, 3, true);
+    char* assistantArray[6] = { "isaac newton\n", "051-6320-123\n", "rene descartes\n", "03240-1234-534\n", "Adam Smith\n", "7(911)-123-45-67\n" };
+    for (int index = 0; index < 5; index += 2)
+    {
+        if (strcmp(testArray[index / 2].name, assistantArray[index]) != 0)
+        {
+            return false;
+        }
+    }
+    for (int index = 1; index < 6; index += 2)
+    {
+        if (strcmp(testArray[index / 2].phone, assistantArray[index]) != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool connectionOfTestingFuctions()
+{
+    FILE* file = fopen("test.txt", "w");
+    if (file == NULL)
+    {
+        printf("File not found");
+        return;
+    }
+
+    fprintf(file, "%s", "isaac newton\n");
+    fprintf(file, "%s", "051-6320-123\n");
+    fprintf(file, "%s", "rene descartes\n");
+    fprintf(file, "%s", "03240-1234-534\n");
+    fclose(file);
+
+    PhoneBookEntry testArray[6];
+    return testReadDataFromFile(testArray) && testAddNote(testArray) && testSearchFunctions(testArray) && testSaveDataToFile(testArray);
+}
+
+int main()
+{
+    if (!connectionOfTestingFuctions())
+    {
+        printf("Test failed");
+        return -1;
+    }
     printf("0 - exit\n1 - add note\n2 - print all notes\n3 - find telephone number using name\n4 - find name using telephone number\n5 - save data to file\n");
     int userInput = 1;
     PhoneBookEntry* notesArray[100];
     int currentIndex = 0;
-    currentIndex = readDataFromFile(notesArray, currentIndex);
+    currentIndex = readDataFromFile(notesArray, currentIndex, false);
     while (userInput != 0)
     {
         printf("\nType any number: ");
         scanf("%d", &userInput);
         getchar(); // used for reading extra newline after scanf
-        if (userInput == 1)
+        if (userInput == 1) //add note
         {
-            addNote(notesArray, currentIndex);
+            char* name[30];
+            printf("\nName: ");
+            fgets(name, 30, stdin);
+            char* phone[30];
+            printf("Phone number: ");
+            fgets(phone, 30, stdin);
+            addNote(notesArray, name, phone, currentIndex);
             currentIndex += 1;
         }
-        if (userInput == 2)
+        if (userInput == 2) //print all notes
         {
             printf("\n-----\n");
             printAllNotes(notesArray, currentIndex);
         }
-        if (userInput == 3)
+        if (userInput == 3) //find telephone number using name
         {
-            findPhoneNumberUsingName(notesArray, currentIndex);
+            char* toSearchName[30];
+            printf("\nName: ");
+            fgets(toSearchName, 30, stdin);
+            const int indexWithWantedNote = findPhoneNumberUsingName(notesArray, toSearchName, currentIndex);
+            if (indexWithWantedNote == -1)
+            {
+                printf("There is no person with this name\n");
+            }
+            else
+            {
+                printNameOrPhoneNumber(notesArray, indexWithWantedNote, false);
+            }
         }
-        if (userInput == 4)
+        if (userInput == 4) //find name using telephone number
         {
-            findNameUsingPhoneNumber(notesArray, currentIndex);
+            char* toSearchPhoneNumber[30];
+            printf("\nPhone number: ");
+            fgets(toSearchPhoneNumber, 30, stdin);
+            const int indexWithWantedNote = findNameUsingPhoneNumber(notesArray, toSearchPhoneNumber, currentIndex);
+            if (indexWithWantedNote == -1)
+            {
+                printf("There is no person with this phone number\n");
+            }
+            else
+            {
+                printNameOrPhoneNumber(notesArray, indexWithWantedNote, true);
+            }
         }
-        if (userInput == 5)
+        if (userInput == 5) //save data to file
         {
-            saveData(notesArray, currentIndex);
+            saveData(notesArray, currentIndex, false);
         }
     }
 }
