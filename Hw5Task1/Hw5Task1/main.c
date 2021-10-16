2c96ca0d172a0168152372935de4d17bd040bf4f
ArtiomPatov <patov.988@gmail.com>
2021-10-16 16:06:50 +0300

    added first version of postfix calculator

---
+++ Hw5Task1/Hw5Task1/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "stack.h"
+
+int main()
+{
+    StackElement* head = NULL;
+    char sequence[30] = { '\0' };
+    printf("Print postfix expression: ");
+    fgets(sequence, (unsigned)sizeof(sequence), stdin);
+    for (int index = 0; index < 30; index++)
+    {
+        if (sequence[index] != ' ' && sequence[index] != '\0' && sequence[index] != '\n')
+        {
+            switch (sequence[index])
+            {
+                case '*':
+                {
+                    push(&head, pop(&head) * pop(&head));
+                    break;
+                }
+                case '/':
+                {
+                    const int divisor = pop(&head);
+                    const int dividend = pop(&head);
+                    push(&head, dividend / divisor);
+                    break;
+                }
+                case '-':
+                {
+                    const int subtrahend = pop(&head);
+                    const int minuend = pop(&head);
+                    push(&head, minuend - subtrahend);
+                    break;
+                }
+                case '+':
+                {
+                    push(&head, pop(&head) + pop(&head));
+                    break;
+                }
+                default:
+                {
+                    push(&head, (int)(sequence[index] - '0'));
+                    break;
+                }
+            }
+        }
+    }
+    printf("%d", pop(&head));
+    deleteStack(&head);
+    return 0;
+}
