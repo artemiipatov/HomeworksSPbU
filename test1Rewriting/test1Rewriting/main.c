04a0c318f3d123793c325f40de3ec8515455876e
ArtiomPatov <patov.988@gmail.com>
2021-10-19 14:58:55 +0300

    added first version of task 3

---
+++ test1Rewriting/test1Rewriting/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "filesWork.h"
+#include "test.h"
+
+int main()
+{
+    if (!testWorkWithFiles())
+    {
+        printf("Tests failed");
+        return -1;
+    }
+    int* array[100] = { 0 };
+    int length = readDataFromFile("f.txt", array);
+    int* number[1] = { 0 };
+    readDataFromFile("g.txt", number);
+    findLessNumbers("h.txt", array, length, number[0]);
+}
