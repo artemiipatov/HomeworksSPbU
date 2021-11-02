86cf15f872df07dd2e973526c89781a1571f87bf
ArtiomPatov <patov.988@gmail.com>
2021-11-02 22:46:40 +0300

    changed comment

--- Hw5Task1/Hw5Task1/calculatorFunctions.h
+++ Hw5Task1/Hw5Task1/calculatorFunctions.h
@@ -1,4 +1,4 @@
 #pragma once
 
-// gets expression and calculates it using operation functions. Returns calculated value
+// gets expression and calculates it using operation functions. Returns calculated result
 int calculate(char sequence[], bool* correctInput, int length);
