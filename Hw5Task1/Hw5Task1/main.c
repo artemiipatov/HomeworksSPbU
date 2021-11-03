c92e483f67088ddb7543aaa8d92ce7ccd407ec33
ArtiomPatov <patov.988@gmail.com>
2021-11-03 10:35:56 +0300

    added hw5task1 files

---
+++ Hw5Task1/Hw5Task1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "../../stack/stack/stackTests.h"
+#include "calculatorFunctions.h"
+#include "calculatorTests.h"
+
+int main()
+{
+    if (!(testsStack() && calculatorTests()))
+    {
+        printf("Tests failed");
+        return -1;
+    }
+    char sequence[30] = { '\0' };
+    printf("Print postfix expression: ");
+    fgets(sequence, (unsigned)sizeof(sequence), stdin);
+
+    bool correctInput = true;
+    int result = calculate(sequence, &correctInput, 30);
+    if (!correctInput)
+    {
+        printf("Incorrect input");
+        return -1;
+    }
+    printf("%d", result);
+    return 0;
+}
